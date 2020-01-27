b33124d66b4fa15baaa7eeceb11e00ad343f9713
NahuelDev <nahuee2011@gmail.com>
2020-01-27 09:13:58 -0300

    fixed the other right space lol

--- scripts/script.js
+++ scripts/script.js
@@ -13,17 +13,12 @@ function size() {
         $(".articleNext,.blog .info p, .bitBlogEntries, .header-text p").addClass("d-none d-lg-block");
         $("header > nav.navbar").removeClass("fixed-top");
         $("main").css("margin-top", "0px");
-        $("article.blog").removeClass("m-5");
-        $("article.blog>h5").addClass("mx-5");
-        $(".blog>.row").removeClass("py-5").addClass("py-2");
+        $("article.tend").addClass("my-5");
         $(".tend>.row>.nuevo").removeClass("col-3").addClass("col-12");
     }
     if ($(window).width() > 992) {
         $("header > nav.navbar").addClass("fixed-top");
         $("main").css("margin-top", "56px");
-        $("article.blog").addClass("m-5");
-        $("article.blog > h5").removeClass("mx-5");
-        $(".blog>.row").addClass("py-5").removeClass("py-2");
         $(".tend>.row>.nuevo").addClass("col-3").removeClass("col-12");
     }
     if ($(window).width() <= 752) {
