c990d5f0e5a9dc4f46fab85e0d4a76e7f1a34492
NahuelDev <nahuee2011@gmail.com>
2020-01-27 10:34:47 -0300

    version for deliver, fixed some bugs like scroll hiden content

--- scripts/script.js
+++ scripts/script.js
@@ -14,27 +14,33 @@ function size() {
         $("header > nav.navbar").removeClass("fixed-top");
         $("main").css("margin-top", "0px");
         $("article.tend").addClass("my-5");
-        $(".tend>.row>.nuevo").removeClass("col-3").addClass("col-12");
+        $(".tend>.row>.nuevo").removeClass("col-3").addClass("col-10");
     }
     if ($(window).width() > 992) {
         $("header > nav.navbar").addClass("fixed-top");
         $("main").css("margin-top", "56px");
-        $(".tend>.row>.nuevo").addClass("col-3").removeClass("col-12");
+        $(".tend>.row>.nuevo").addClass("col-3").removeClass("col-10");
     }
     if ($(window).width() <= 752) {
         $(".tendThumbnails:not(.nuevo)").addClass("d-none d-lg-block");
-        $(".tend>.row>.nuevo").removeClass("col-3").addClass("col-12");
+        $(".tend>.row>.nuevo").removeClass("col-3").addClass("col-10");
     }
     if ($(window).width() > 752) {
         $(".tendThumbnails:not(.nuevo)").removeClass("d-none d-lg-block");
-        $(".tend>.row>.nuevo").addClass("col-3").removeClass("col-12");
+        $(".tend>.row>.nuevo").addClass("col-3").removeClass("col-10");
     }
 }
 
-/*change the active in the navbar depending the click*/
+/*change the active in the navbar depending the click and add an offset for the fixed header*/
 
 $(document).ready(function() {
     $('.navbar-nav .nav-item').click(function() {
+        if ($(window).width() > 992) {
+            event.preventDefault()
+            $('html, body').animate({
+                scrollTop: $($(this).find("a[href]").attr('href')).offset().top - 56
+            }, 500);
+        }
         $('.navbar-nav .nav-item').removeClass('active');
         $(this).addClass('active');
     })
